84092edf6ff2d389b0e05fd93a81f1324e4ea031
Bas Alberts <bas@mokusatsu.org>
2019-07-12 21:23:03 -0400

    v0.3: make better use of existing ghidra reference API

--- StringGroup.java
+++ StringGroup.java
@@ -14,7 +14,6 @@ import ghidra.app.tablechooser.AddressableRowObject;
 import ghidra.app.tablechooser.ColumnDisplay;
 import ghidra.app.tablechooser.StringColumnDisplay;
 import ghidra.app.tablechooser.TableChooserDialog;
-import ghidra.program.model.block.*;
 import ghidra.program.model.symbol.*;
 import ghidra.program.model.listing.*;
 import ghidra.program.model.address.*;
@@ -23,18 +22,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class StringGroup extends GhidraScript {
         
     private Listing listing;
-    private HashMap<Address, Data> stringMap;
-    private HashMap<Address, Data> functionReferenceMap;
     private File logFile;
     private PrintWriter logWriter;
-    private IsolatedEntrySubModel subModel;
     private TableChooserDialog tableDialog;
     private boolean emptyTable;
+    
+    private HashMap<Function, ArrayList<Map.Entry<Instruction, Data>>> functionReference;
+    private HashMap<Instruction, Data> nonFunctionReference;
         
     private void logLine(String line) {
         if (logWriter != null) {
@@ -44,64 +46,32 @@ public class StringGroup extends GhidraScript {
         return;
     }
     
-    private void checkNonFunctionInstructions() throws Exception {
-        // only call this when functionReferenceMap has been fully populated
-        InstructionIterator instructionsIter = listing.getInstructions(currentProgram.getMemory(), true);
-        while (instructionsIter.hasNext() && !monitor.isCancelled()) {
-            Instruction instruction = instructionsIter.next();
-            // skip instructions that are part of our established function references
-            if (functionReferenceMap.containsKey(instruction.getAddress())) {
-                continue;
-            }
-            Reference[] references = instruction.getReferencesFrom();
-            for (Reference ref : references) {
-                Address iAddress = instruction.getAddress();
-                Address rAddress = ref.getToAddress(); 
-                if (stringMap.containsKey(rAddress)) {
-                    Data sData = stringMap.get(rAddress);
-                    String sValue = sData.getDefaultValueRepresentation();
-                    logLine("!!! Non-Function String Ref @" + iAddress + ": " + sValue);
-                    tableDialog.add(new FuncStringRef(null, iAddress, sValue, sData.getAddress()));
-                }
-            }
+    private void checkNonFunctionReferences() throws Exception {
+        for (Map.Entry<Instruction, Data> xref : nonFunctionReference.entrySet()) {
+			Address xrefAddress = xref.getKey().getAddress();
+			Address stringAddress = xref.getValue().getAddress();
+			String stringValue = xref.getValue().getDefaultValueRepresentation();
+            logLine(xrefAddress + ": " + stringValue);
+            logLine("### non-function reference @" + xrefAddress + ": " + stringValue);
+            tableDialog.add(new FuncStringRef(null, xrefAddress, stringValue, stringAddress));
+            emptyTable = false;
         }
     }
     
     private void checkFunction(Function f) throws Exception {
-        boolean functionRefsStrings = false;
-        CodeBlockIterator subIter = subModel.getCodeBlocksContaining(f.getBody(), monitor);
-        while (subIter.hasNext()) {
-            // get a single code block
-            CodeBlock block = subIter.next();
-            // walk the addresses for the code block
-            AddressIterator blockAddresses = block.getAddresses(true);
-            while (blockAddresses.hasNext()) {
-                Address blockAddress = blockAddresses.next();
-                // check if the instruction has a reference to any known string
-                Instruction instruction = listing.getInstructionAt(blockAddress);
-                if (instruction == null) {
-                    continue;
-                }
-                Reference[] references = instruction.getReferencesFrom();
-                for (Reference ref : references) {
-                    if (stringMap.containsKey(ref.getToAddress())) {
-                        if (functionRefsStrings == false) {
-                            logLine("### " + f.getName());
-                            functionRefsStrings = true;
-                            emptyTable = false;
-                        }
-                        Data stringData = stringMap.get(ref.getToAddress());
-                        functionReferenceMap.put(blockAddress, stringData);
-                        String stringValue = stringData.getDefaultValueRepresentation();
-                        logLine(blockAddress + ": " + stringValue);
-                        tableDialog.add(new FuncStringRef(f, blockAddress, stringValue, stringData.getAddress()));
-                    }
-                }
-            }
-        }
-        if (functionRefsStrings == true) {
-            logLine("");
-        }
+    	if (functionReference.containsKey(f)) {
+    		logLine("### " + f.getName());
+    		ArrayList<Map.Entry<Instruction, Data>> xrefs = functionReference.get(f);
+    		for (Map.Entry<Instruction, Data> xref : xrefs) {
+    			Address xrefAddress = xref.getKey().getAddress();
+    			Address stringAddress = xref.getValue().getAddress();
+    			String stringValue = xref.getValue().getDefaultValueRepresentation();
+                logLine(xrefAddress + ": " + stringValue);
+                tableDialog.add(new FuncStringRef(f, xrefAddress, stringValue, stringAddress));
+                emptyTable = false;
+    		}
+    		logLine("");
+    	}
     }
     
     private boolean openLog() throws Exception {
@@ -122,13 +92,36 @@ public class StringGroup extends GhidraScript {
         return true;
     }
     
-    public void getStringAddresses() throws Exception {
+    public void getStringReferences() throws Exception {
         DataIterator dataIter = listing.getDefinedData(true);
         while (dataIter.hasNext() && !monitor.isCancelled()) {
             Data data = dataIter.next();
             String type = data.getDataType().getName().toLowerCase();
+            // XXX: see if there is a better API for determining string data types
             if ((type.contains("unicode") || type.contains("string"))) {
-                stringMap.put(data.getAddress(), data);
+            	ReferenceIterator refIter = data.getReferenceIteratorTo();
+            	while (refIter.hasNext() && !monitor.isCancelled()) {
+            		Address fromAddress = refIter.next().getFromAddress();
+            		Function f = listing.getFunctionContaining(fromAddress);
+            		Instruction i = listing.getInstructionAt(fromAddress);
+            		// matched reference to an instruction belonging to a function
+            		if (f != null && i != null) {
+            			Map.Entry<Instruction, Data> xref = new AbstractMap.SimpleEntry<>(i, data);
+            			if (functionReference.containsKey(f)) {
+            				functionReference.get(f).add(xref);
+            			} else {
+            				ArrayList<Map.Entry<Instruction, Data>> xrefList = new ArrayList<>();
+            				xrefList.add(xref);
+            				functionReference.put(f, xrefList);
+            			}
+            		}
+            		// matched reference to an instruction not belonging to a function
+            		else if (i != null) {
+            			nonFunctionReference.put(i, data);
+            		} else {
+            			logLine("###: no function or instruction for string reference @" + fromAddress);
+            		}
+            	}
             }
         }        
     }  
@@ -142,9 +135,8 @@ public class StringGroup extends GhidraScript {
         
         // things we need for our analysis
         listing = currentProgram.getListing();
-        subModel = new IsolatedEntrySubModel(currentProgram);
-        stringMap = new HashMap<>();
-        functionReferenceMap = new HashMap<>();
+        functionReference = new HashMap<>();
+        nonFunctionReference = new HashMap<>();
                         
         // create a table to toss results into
         tableDialog = createTableChooserDialog(currentProgram.getName() + ": grouped string references", null);
@@ -153,30 +145,25 @@ public class StringGroup extends GhidraScript {
 
         // handle single function if current address is a function ... give option to handle all functions if not
         if (currentAddress != null && listing.getFunctionContaining(currentAddress) != null) {
-            getStringAddresses();
+            getStringReferences();
             checkFunction(listing.getFunctionContaining(currentAddress));
            
         }
         // do a full analysis run
         else if (askYesNo("No Current Function", "Process all Functions?")) {
-            
             if (askYesNo("Log Results", "Log results to disk?")) {
                 openLog();
             } else {
                 logWriter = null;
             }
-            
-            getStringAddresses();
+            getStringReferences();
             FunctionIterator functionIter = listing.getFunctions(true);
             while (functionIter.hasNext() && !monitor.isCancelled()) {
                 checkFunction(functionIter.next());    
-            }    
-            
-            if (askYesNo("Check Non-Function Instructions", "Check for Non-Function String References?")) {
-                checkNonFunctionInstructions();
-            }
-            
-        } else return;
+            }   
+            checkNonFunctionReferences();
+        } else 
+        	return;
         		
         if (logWriter != null) {
             logWriter.close();
@@ -186,6 +173,8 @@ public class StringGroup extends GhidraScript {
         // don't be annoying and only pop up a table when we have results to show
         if (emptyTable == false) {
             tableDialog.show();
+        } else {
+        	tableDialog.close();
         }
    		
         return;
